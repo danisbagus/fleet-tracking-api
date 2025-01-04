8b2b2eecd13e151ad41aa5c6b3856aaf609db17a
Danis Bagus Setiawan <danis.bagus@majoo.id>
2025-01-04 16:28:16 +0700

    initial commit

---
+++ src/main/java/danisbagus/fleet_tracking_api/FleetTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package danisbagus.fleet_tracking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetTrackingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/danisbagus/fleet_tracking_api/FleetTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package danisbagus.fleet_tracking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
