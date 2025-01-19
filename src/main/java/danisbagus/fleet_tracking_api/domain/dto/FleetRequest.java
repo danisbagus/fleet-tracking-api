0adf9b40c319956e45bbbf4867f611fb192d94ba
Danis Bagus Setiawan <danis.bagus@majoo.id>
2025-01-19 22:02:07 +0700

    feat(exception): add exception handler for argument not valid

--- src/main/java/danisbagus/fleet_tracking_api/domain/dto/FleetRequest.java
+++ src/main/java/danisbagus/fleet_tracking_api/domain/dto/FleetRequest.java
@@ -3,6 +3,7 @@ package danisbagus.fleet_tracking_api.domain.dto;
 import danisbagus.fleet_tracking_api.domain.enums.VehicleType;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class FleetRequest {
@@ -10,6 +11,7 @@ public class FleetRequest {
     @Size(min = 4, message = "Vehicle number must be at least 4 characters long")
     private String vehicleNumber;
 
+    @NotNull(message = "Vehicle type cannot be null")
     private VehicleType vehicleType;
 
     public String getVehicleNumber() {
