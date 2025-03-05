eb51a37964d3d1a18ef9e6c2ebbdb1c2667cd1dd
danisbagus <danis@machtwatch.co.id>
2025-03-05 09:13:05 +0700

    feat: cleanup

--- src/main/java/danisbagus/fleet_tracking_api/service/CustomUserDetailsService.java
+++ src/main/java/danisbagus/fleet_tracking_api/service/CustomUserDetailsService.java
@@ -1,20 +1,12 @@
 package danisbagus.fleet_tracking_api.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import danisbagus.fleet_tracking_api.domain.entity.UserEntity;
-// import danisbagus.fleet_tracking_api.domain.entity.UserEntity;
 import danisbagus.fleet_tracking_api.repository.UserRepository;
-
-// import java.lang.StackWalker.Option;
-// import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
